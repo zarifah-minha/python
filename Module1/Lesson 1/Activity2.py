0ebf6db772bc090a0873ca0fd858a41936f10148
zarifah-minha <zarifahaminminha@gmail.com>
2026-06-04 19:40:01 +0600

    Daily Routine

---
+++ Module1/Lesson 1/Activity2.py
@@ -0,0 +1,24 @@
+# Head
+print ("My Daily Routine.")
+# Step : 1
+print ("Everyday I wake up at 6:30 AM.")
+# Step : 2
+print ("Then I freshen up, brush, put my school uniform on and eat breakfast.")
+# Step : 3
+print ("After I take my school bag and go to school at 7:30 AM.")
+# Step : 4
+print ("I come back from school at 1 AM.")
+# Step : 4
+print ("Then I just chill for about 1 hour and take a bath.")
+# Step : 5
+print ("After taking a bath, I eat lunch .")
+# Step : 6
+print ("Then I sleep or just use my phone.")
+# Step : 7
+print ("At 5 PM, I start studying. And study until Maghrib Azan.")
+# Step : 8 
+print ("Then I take 1-hour break AGAIN!")
+# Step : 9
+print ("After taking a break, I study until 11 PM.")
+# Step : 10
+print ("Afte 11 PM, I enjoy 'me time' by scroling through phone.")
