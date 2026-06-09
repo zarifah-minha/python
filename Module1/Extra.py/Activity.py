7a3e1fbf9c151df817cc29e684ea6425fcf3c3d7
zarifah-minha <zarifahaminminha@gmail.com>
2026-06-09 19:45:37 +0600

    Birthdays

---
+++ Module1/Extra.py/Activity.py
@@ -0,0 +1,29 @@
+# 1st Step
+print("Hello, my name is Zarifah Amin Minha.")
+
+# 2nd Step
+print("I read in class 8.")
+
+# 3rd Step
+print("I am 14 years old")
+
+# 4th Step
+print("I live in Uttara Dhaka.")
+
+# 5th Step
+print("I love to read books. Some of them are: \n1.Wimpy Kid 2.Tom Gates etc.")
+
+# 6th Step
+print("I also like to eat, sleep.")
+
+# 7th Step
+print("I am good at sports.")
+
+# 8th Step
+print("I am not that good in studies.")
+
+# 9th Step
+print("I like to annoy my sister.")
+
+#10th Step
+print("I also like to annoy my mother.")
---
+++ Module1/Lesson 2/Activity4.py
@@ -0,0 +1,5 @@
+Arya = input("Enter Arya's birthday: ")
+Saini = input("Enter Saini's birthday: ")
+Fatima = input("Enter Fatima's birthday: ")
+Tanzeem = input("Enter Tanzeem's birthday: ")
+
