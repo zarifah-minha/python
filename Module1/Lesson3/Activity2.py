73d1e7f0f18d52719ba7b16329f176d44354dc7c
zarifah-minha <zarifahaminminha@gmail.com>
2026-06-13 21:16:06 +0600

    typecasting

---
+++ Module1/Lesson3/Activity2.py
@@ -0,0 +1,20 @@
+a = 5
+print(type(a))
+
+b = 5.55
+print(type(b))
+
+name = "Tasmia"
+print(type(name))
+
+x = False
+print(type(x))
+
+#Typecasting
+b = int(b)
+print (b)
+print(type(a))
+
+x = float(x)
+print(x)
+print(type(x))
