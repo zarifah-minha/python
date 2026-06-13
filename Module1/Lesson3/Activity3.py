0a710f29ef862117d9d32ab0504a2e7eda437a2e
zarifah-minha <zarifahaminminha@gmail.com>
2026-06-13 21:29:14 +0600

    String operation

---
+++ Module1/Lesson3/Activity3.py
@@ -0,0 +1,19 @@
+word = input("Enter the word: ")
+
+#Length
+print("Lenght:",len(word))
+
+#Indexing
+print(word[4])
+
+#Slicing
+print(word[0:2])
+
+#Upppercase
+print(word.upper())
+
+#Lowercase
+print(word.lower())
+
+#Reverse
+print(word[:: -1])
