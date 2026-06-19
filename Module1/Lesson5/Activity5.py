da56012a9b8e3112444c5c74ef75b316a76511d0
zarifah-minha <zarifahaminminha@gmail.com>
2026-06-19 21:42:20 +0600

    Temperature

---
+++ Module1/Lesson5/Activity5.py
@@ -0,0 +1,6 @@
+temperature = int(input("Enter the temperature: "))
+
+if temperature < 25: 
+    print("You can wear jacket and pullover")
+else:
+    print("You don't need to wear jacket and pullover")
