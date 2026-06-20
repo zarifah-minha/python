ff8121423195717b0131d0fd95a1bc317271e0c9
zarifah-minha <zarifahaminminha@gmail.com>
2026-06-20 21:20:24 +0600

    AND OR NOT

---
+++ Module1/Lesson6/Activity.py
@@ -0,0 +1,11 @@
+x = 16
+y = 21
+
+if x>0 and x%2==0:
+    print(f"{x} is a positive and an even number")
+
+if y<0 or y%2 !=0:
+    print("At lest one of the conndition is true")   
+
+if not (x==y):
+    print(f"{x} and {y} are not equal")
