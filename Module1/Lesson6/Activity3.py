0dde99fdc9906d04886ce928b26341ff0d5789c7
zarifah-minha <zarifahaminminha@gmail.com>
2026-06-26 22:34:51 +0600

    ALPHA OR NOT

---
+++ Module1/Lesson6/Activity3.py
@@ -0,0 +1,6 @@
+character = input("Enter any thing to see if it's alphabet or not:")
+
+if ('A' <= character <= 'Z') or ('a' <= character <= 'z'):
+    print("It is an alphabet.")
+else:
+    print("It is not an alphabet.")
