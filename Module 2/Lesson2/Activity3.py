00ed4c4e5d9a5ed1539a71295129ea9c30af925a
zarifah-minha <zarifahaminminha@gmail.com>
2026-07-01 23:13:13 +0600

    POWER

--- Module 2/Lesson2/Activity3.py
+++ Module 2/Lesson2/Activity3.py
@@ -1,4 +1,4 @@
 n = int(input("Enter your number: "))
 
-for i in range(n, 0, -1):
+for i in range(n,0, -1):
     print(i)
---
+++ Module 2/Lesson2/Activity4.py
@@ -0,0 +1,6 @@
+n = int(input("Enter your number: "))
+product= 1
+for i in range(1,n+1):
+  product= product*n;
+ 
+print(f"Your power number is: {product}")
