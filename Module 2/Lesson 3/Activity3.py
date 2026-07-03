e8aa834f9b56ccba97092aa0b5871e8c220c3847
zarifah-minha <zarifahaminminha@gmail.com>
2026-07-03 21:27:49 +0600

    ARMSTRONG NUMBER

---
+++ Module 2/Lesson 3/Activity3.py
@@ -0,0 +1,12 @@
+n = int(input("Enter a number: "))
+sum = 0
+temp = n
+while temp > 0:
+    digit =  temp % 10
+    sum = sum + digit**3
+    temp = temp//10
+if n == sum:
+    print(f"{n} is an armstrong number")
+
+else:
+    print("not armstrong")
