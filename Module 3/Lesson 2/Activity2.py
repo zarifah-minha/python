39bbd662aa8b8d6c11c20f723a5f8f6fd51e9c3a
zarifah-minha <zarifahaminminha@gmail.com>
2026-07-25 10:22:15 +0600

    CUBE

---
+++ Module 3/Lesson 2/Activity2.py
@@ -0,0 +1,11 @@
+def cube(number):
+    return number**3
+
+def by_three(number):
+    if number%3 == 0 :
+        return cube(number)
+    else:
+        return "Invalid"
+    
+number = int(input("Enter a number: "))
+print(by_three(number))
