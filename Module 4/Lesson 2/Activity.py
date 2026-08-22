b89199fd79b9ac73d907bc0b4dc1f83eee3f837d
zarifah-minha <zarifahaminminha@gmail.com>
2026-08-22 12:15:56 +0600

    TUPLE

---
+++ Module 4/Lesson 2/Activity.py
@@ -0,0 +1,17 @@
+my_tuple = ("codingal", 2006, 34.55, True)
+print(my_tuple)
+
+#Lenght
+print(len(my_tuple))
+
+#Accesing an item
+print(my_tuple[1])
+print(my_tuple[0])
+print(my_tuple[-1])
+
+#Slicing an item
+print(my_tuple[1:4])
+
+# Adding an new item
+my_tuple = my_tuple + (9,)
+print(my_tuple)
