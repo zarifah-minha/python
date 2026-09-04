2919af00c129c503b537e4fb8ce3cfed5d6c8929
zarifah-minha <zarifahaminminha@gmail.com>
2026-09-04 09:45:54 +0600

    country code

---
+++ Module 4/Lesson 3/Activity3.py
@@ -0,0 +1,8 @@
+country_code = {
+    "India" : "0091",
+    "Australia" : "0025",
+    "Nepal": "00097"
+}
+
+print(country_code.get("Nepal", "Not found"))
+print(country_code.get("Japan", "Not found")) 
